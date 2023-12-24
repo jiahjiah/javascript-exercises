09045c7e0aae2ef738bd8831b54f172c77741e86
Jia <yijh1705@gmail.com>
2023-12-23 20:43:54 -0500

    Finished exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(string, times) {
+    let returnedString = "";
 
+    if (times < 0 ) return "ERROR";
+
+    for (let i = 0; i < times; i++) {
+        returnedString += string;
+    }    
+     
+    return returnedString;
 };
 
 // Do not edit below this line
